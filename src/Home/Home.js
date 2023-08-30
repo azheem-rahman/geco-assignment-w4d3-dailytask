37359148247124ce6c293c04ba146b793f2b5a10
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-31 03:11:56 +0800

    Home page done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { getPostsAPI } from "../assets/services";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
+import FavouriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
 
 const Home = () => {
-  return <div></div>;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(getPostsAPI)
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <React.Fragment>
+      {posts ? (
+        <div>
+          <Typography variant="h4" style={{ margin: "1rem" }}>
+            Latest Blog Posts
+          </Typography>
+          {posts.map((post) => {
+            return (
+              <Card
+                sx={{ maxWidth: "100%" }}
+                key={post.id}
+                variant="outlined"
+                style={{ padding: "1rem", margin: "1rem" }}
+              >
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {post.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {post.body}
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favourites">
+                    <FavouriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            );
+          })}{" "}
+        </div>
+      ) : (
+        <div>
+          <LinearProgress color="inherit" />
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default Home;
---
+++ src/assets/services.js
@@ -0,0 +1,1 @@
+export const getPostsAPI = "https://jsonplaceholder.typicode.com/posts/";
