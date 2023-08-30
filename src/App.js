cc0a6b3f3e451671e97fbc4aacb2b7d6bd2becdf
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-31 01:52:22 +0800

    created Navbar About Contact Home SignUp components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes></Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 };
 
