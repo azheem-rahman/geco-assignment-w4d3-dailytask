d0e0d33a3b0f889a61d8039dcc98c58f7d1405d3
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-31 11:35:33 +0800

    Contact Us component done

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div></div>;
+  const [contactData, setContactData] = useState([
+    {
+      location: "Singapore",
+      contactNum: "(65) 1234-5678",
+      email: "singapore@email.com",
+    },
+    {
+      location: "Malaysia",
+      contactNum: "(60) 12-345-6789",
+      email: "malaysia@email.com",
+    },
+    {
+      location: "Indonesia",
+      contactNum: "(62) 123-4567-8999",
+      email: "indonesia@email.com",
+    },
+  ]);
+
+  return (
+    <React.Fragment>
+      <Typography variant="h4" style={{ margin: "1rem" }}>
+        Contact Us
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {contactData.map((contact, index) => {
+          return (
+            <Card key={index} sx={{ m: 1, p: 1 }}>
+              <CardContent>
+                <Typography>{contact.location}</Typography>
+                <Typography>{contact.contactNum}</Typography>
+                <Typography>{contact.email}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Box>
+    </React.Fragment>
+  );
 };
 
 export default Contact;
